81a234d5f777f978c8b4c36046013cdb41e01140
Manikandan063 <manikandanthangaraj06@gmail.com>
2024-10-15 10:20:44 +0530

    Add files via upload

---
+++ Css/portfolio.js
@@ -0,0 +1,13 @@
+
+  function handleSubmit(event) {
+    event.preventDefault(); 
+
+    
+    document.getElementById("contactForm").reset();
+    
+  
+    alert("Thank you for your message! We will get back to you shortly.");
+  }
+
+
+
